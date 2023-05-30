771384cf42a683a7a0508c8c5c3342fc206dfc03
Dmitry Podkoritov <mitrofun1990@gmail.com>
2023-05-31 00:37:21 +0500

    add pallindrom

---
+++ pallindrome/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число: ");
+int numMy123 = int.Parse(Console.ReadLine()!);
+    
+int first1 = numMy123 % 10;
+int second2 = first1 % 10;
+int third3 = second2 % 10;
+int fourth4 = third3 % 10;
+int fifth5 = fourth4 % 10;
+
+void FindPall(int first1, int second2, int third3, int fourth4, int fifth5)
+{
+    Console.WriteLine($"{fifth5}{fourth4}{third3}{second2}{first1}");
+
+    if (fifth5 == first1 && fourth4 == second2)
+{
+    Console.WriteLine("Данное число является паллиндромом!!!");
+}
+    else 
+{
+    Console.WriteLine("Error");
+}
+}
+int number = numMy123;
+FindPall(number);
