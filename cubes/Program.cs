117dc0d98471ee30f3e078c3c79367918a7743e3
Dmitry Podkoritov <mitrofun1990@gmail.com>
2023-05-31 00:43:05 +0500

    add cubes

---
+++ cubes/Program.cs
@@ -0,0 +1,28 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    string num11 = Console.ReadLine()!;
+    
+
+// Проверка
+
+if ((int.TryParse(num11, out int num_n))== false)
+{
+    Console.WriteLine("Это не число: ");
+    
+}
+
+return num_n;
+}
+
+void PrintDegree(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        Console.Write(Math.Pow(i,3) + " ");
+    }
+}
+int num11 = Prompt("Введите число N: ");
+int number = Math.Abs(num11);
+PrintDegree(number);
+
